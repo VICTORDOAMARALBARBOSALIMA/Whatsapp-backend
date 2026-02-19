97ed9f07eaaa2e1fa96e63d73de096f0ac52ea0e
VICTORDOAMARALBARBOSALIMA <victor.ablima@gmail>
2026-02-19 16:45:00 -0300

    instal.sh adicionado

--- services/whatsappService.js
+++ services/whatsappService.js
@@ -48,6 +48,8 @@ async function initSession(clinic_id) {
     }),
     puppeteer: {
       headless: true,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+
       args: [
         '--no-sandbox',
         '--disable-setuid-sandbox',
