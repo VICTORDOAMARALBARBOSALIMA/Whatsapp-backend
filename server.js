9111f6e28c6f8f919a8b244f453335d47cbcc401
VICTORDOAMARALBARBOSALIMA <victor.ablima@gmail>
2026-02-19 16:19:39 -0300

    cors atualizado

--- server.js
+++ server.js
@@ -13,10 +13,9 @@ const PORT = process.env.PORT || 3000;
 // CORS configurado
 // -------------------------
 app.use(cors({
-  origin: ['https://formulape2.mocha.app', 'http://localhost:5173'],
+  origin: ['https://formulape2.mocha.app', 'https://app.formulape.app.br/whatsapp', 'http://localhost:5173'],
   credentials: true
 }));
-app.use(cors({ origin: 'https://formulape2.mocha.app' }));
 // -------------------------
 // Body parser
 // -------------------------
